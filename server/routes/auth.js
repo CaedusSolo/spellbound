802202cc6597c3a48246516ea21eaa45d6d9bfc3
Ying Tong <yingtong0106@gmail.com>
2024-11-07 22:00:35 +0800

    auth: implement working bare login feature

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -39,4 +39,28 @@ router.post("/register", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      username: req.body.username,
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: "User does not exist." });
+    }
+
+    bcrypt.compare(req.body.password, user.password, (err, result) => {
+      if (result) {
+        return res.status(200).json({ message: "Logged In Successfully." });
+      }
+
+      return res.status(401).json({ error: "Passwords do not match." });
+    });
+  } catch (err) {
+    return res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
+});
+
 export default router;
--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,39 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function LoginPage() {
+  const initialFormData = {
+    username: "",
+    password: "",
+  };
+  const [formData, setFormData] = useState(initialFormData);
+
+  function handleInputChange(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  async function handleFormSubmit(e) {
+    e.preventDefault();
+
+    console.log("Form submitted!")
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/auth/login",
+        {
+          username: formData.username,
+          password: formData.password
+        }
+      );
+      alert(`Message: ${response.data.message}`)
+    } catch (err) {
+      alert(`Error: ${err.response.data.error}`);
+    }
+  }
+
   return (
     <section className="content-container loginPage p-3">
       <h2 className="text-center">Log In</h2>
-      <form action="post">
-
+      <form onSubmit={handleFormSubmit}>
         <div className="form-group mt-4 w-75 d-block mx-auto">
           <label htmlFor="username" className="form-label">
             Username
           </label>
           <input
-            type="email"
+            type="text"
             className="form-control"
             name="username"
             id="username"
+            value={formData.username}
+            minLength={4}
             aria-describedby="usernameHelp"
             placeholder="Enter username"
+            onChange={handleInputChange}
+            required
           />
         </div>
 
         <div className="form-group mt-4 w-75 d-block mx-auto">
-          <label htmlFor="password1" className="form-label">
+          <label htmlFor="password" className="form-label">
             Enter Password
           </label>
           <input
             type="password"
             className="form-control"
-            name="password1"
-            id="password1"
+            name="password"
+            id="password"
+            value={formData.password}
             aria-describedby="password1Help"
             placeholder="Enter password"
+            onChange={handleInputChange}
+            required
           />
         </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+        <button type="submit" className="btn btn-primary mt-5">
+          Submit
+        </button>
       </form>
     </section>
   );
